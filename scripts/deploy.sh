beefb5e5cd5ac655ce915d480c5aed2f7ed1daae
Thom Behrens <tbehrens@appirio.com>
2019-08-03 12:50:47 -0400

    deployment scripts dynamic variables

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,13 +3,16 @@
 # Set Audience - https://salesforce.stackexchange.com/a/253940
 export SFDX_AUDIENCE_URL=https://test.salesforce.com
 
-#Authorize target org
+#Authorize target org - This depends on the consumer key and username CCI environment variables being named after the branch associated with them, i.e.:
+# if deploying from branch DEV_INT,
+# Consumer Key variable must be named DEV_INT_CONSUMER_KEY and
+# Username variable must be named DEV_INT_USER_NAME
 
-sfdx force:auth:jwt:grant --instanceurl $ENDPOINT  --clientid $CONSUMER_KEY --jwtkeyfile assets/server.key --username $USER_NAME --setalias UAT
+sfdx force:auth:jwt:grant --instanceurl $ENDPOINT  --clientid $CIRCLE_BRANCH\_CONSUMER_KEY --jwtkeyfile assets/server.key --username $CIRCLE_BRANCH\_USER_NAME --setalias DEPLOYMENT_ORG
 
 #Deploy as sfdx source - this is a quicker deployment that should be used for non-production orgs
-sfdx force:source:deploy -p force-app/main/default/ -u $USER_NAME
+sfdx force:source:deploy -p force-app/main/default/ -u $CIRCLE_BRANCH\_USER_NAME
 
 ##Convert & deploy as mdapi - this is a longer deployment, should be used for prod deployments, as it is more stable. Can be tracked through "Deployment Status" in the target org.
 #sudo sfdx force:source:convert -r force-app -d /deploy
-#sfdx force:mdapi:deploy --wait 10 --deploydir /deploy --targetusername UAT --testlevel $TESTLEVEL
+#sfdx force:mdapi:deploy --wait 10 --deploydir /deploy --targetusername DEPLOYMENT_ORG --testlevel $TESTLEVEL
