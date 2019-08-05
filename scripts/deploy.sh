beef2789ae6136c90d214ba93e0df580f6fc5ec0
Thom Behrens <tbehrens@appirio.com>
2019-08-05 11:54:53 -0400

    updating to show mdapi example

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,8 +14,8 @@ USER_NAME=$CIRCLE_BRANCH\_USER_NAME
 sfdx force:auth:jwt:grant --instanceurl $ENDPOINT  --clientid ${!CONSUMER_KEY} --jwtkeyfile assets/server.key --username ${!USER_NAME} --setalias DEPLOYMENT_ORG
 
 #Deploy as sfdx source - this is a quicker deployment that should be used for non-production orgs
-sfdx force:source:deploy -p force-app/main/default/ -u DEPLOYMENT_ORG
+#sfdx force:source:deploy -p force-app/main/default/ -u DEPLOYMENT_ORG
 
 ##Convert & deploy as mdapi - this is a longer deployment, should be used for prod deployments, as it is more stable. Can be tracked through "Deployment Status" in the target org.
-#sudo sfdx force:source:convert -r force-app -d /deploy
-#sfdx force:mdapi:deploy --wait 10 --deploydir /deploy --targetusername DEPLOYMENT_ORG --testlevel $TESTLEVEL
+sudo sfdx force:source:convert -r force-app -d /deploy
+sfdx force:mdapi:deploy --wait 10 --deploydir /deploy --targetusername DEPLOYMENT_ORG --testlevel $TESTLEVEL
